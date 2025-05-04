7ee7e9d57d1924333c57fe8f671a16f06ac67934
Derek <wmaungyadanar@gmail.com>
2025-05-04 15:19:49 +0630

    added div squares grids and create a function to blacken the div when it is hover by cousor

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.getElementById("container");
 
-
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < 256; i++){
     let square = document.createElement("div");
     square.setAttribute("id", "square");
+    square.setAttribute("class", "square");
+    square.addEventListener("mouseenter", () =>{
+        square.style.backgroundColor = "black";
+    }
+    )
     container.appendChild(square);
-
+    
 }
+
