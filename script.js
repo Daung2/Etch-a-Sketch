0dd064d5e94481dba6ea98beb91e1c3c73346928
Derek <wmaungyadanar@gmail.com>
2025-05-05 15:20:14 +0630

    Modified the square layout flexible

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const container = document.getElementById("container");
+var horizontal = 100;
+var vertical = 100;
+var totalSquare = horizontal * vertical;
 
-for(let i = 0; i < 256; i++){
+for(let i = 0; i < totalSquare; i++){
     let square = document.createElement("div");
+    let length = 100/horizontal;
+    let lengthContainer = length * horizontal;
     square.setAttribute("id", "square");
     square.setAttribute("class", "square");
+    container.style.width = `${lengthContainer}vh`;
+    square.style.width = `${length}vh`;
+    square.style.height = `${length}vh`;
     square.addEventListener("mouseenter", () =>{
         square.style.backgroundColor = "black";
     }
     )
     container.appendChild(square);
-    
 }
+
+
 
