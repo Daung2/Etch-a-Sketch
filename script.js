51dc18123868f1e8af379d10855e82029d464945
Derek <wmaungyadanar@gmail.com>
2025-05-08 14:07:28 +0630

    modifed to be able to change the block amounts

--- script.js
+++ script.js
@@ -1,22 +1,46 @@
 const container = document.getElementById("container");
-var horizontal = 100;
-var vertical = 100;
-var totalSquare = horizontal * vertical;
+const block = document.getElementById("square");
+const adjust = document.getElementById("adjust");
+const pixNumber = document.getElementById("pixNumber");
 
-for(let i = 0; i < totalSquare; i++){
-    let square = document.createElement("div");
-    let length = 100/horizontal;
-    let lengthContainer = length * horizontal;
-    square.setAttribute("id", "square");
-    square.setAttribute("class", "square");
-    container.style.width = `${lengthContainer}vh`;
-    square.style.width = `${length}vh`;
-    square.style.height = `${length}vh`;
-    square.addEventListener("mouseenter", () =>{
-        square.style.backgroundColor = "black";
+var horizontal = 100;
+var totalSquare = horizontal **2;
+
+function changePixel() {
+    removeBlock()
+    horizontal = pixNumber.value;
+    totalSquare = horizontal **2;
+    pixNumber.value="";
+    generateBlock();
+
+};
+
+addEventListener("load",()=>{
+    generateBlock();
+})
+
+function removeBlock(){
+   while(container.firstChild) {
+    container.removeChild(container.firstChild);
+   }
+}
+
+function generateBlock(){
+    for(let i = 0; i < totalSquare; i++){
+        let square = document.createElement("div");
+        let length = 100/horizontal;
+        let lengthContainer = length * horizontal;
+        square.setAttribute("id", "square");
+        square.setAttribute("class", "square");
+        container.style.width = `${lengthContainer}vh`;
+        square.style.width = `${length}vh`;
+        square.style.height = `${length}vh`;
+        square.addEventListener("mouseenter", () =>{
+            square.style.backgroundColor = "black";
+        }
+        )
+        container.appendChild(square);
     }
-    )
-    container.appendChild(square);
 }
 
 
